11c57c36ed4f0c17349a5e842154a8140adbf097
Pantheeon <neilrayu@gmail.com>
2022-07-22 22:08:11 -0500

    added microscope button, and other style

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 fieldSpots = document.getElementsByClassName('fieldSpot');
+let geneViewMode = false;
+
 fieldSpots[0].onclick = () => {
-  document.getElementById('myForm').style.display = 'flex';
+  if (geneViewMode) {
+    //
+  } else {
+    document.getElementById('myForm').style.display = 'flex';
+  }
 };
 function removeOpt() {
   //e.preventDefault();
   document.getElementById('myForm').style.display = 'none';
   let img = document.createElement('img');
   img.src = 'flower1200.png';
-  img.style = 'position: absolute; top: 0; left: 0;';
+  img.style = 'position: absolute; top: 30px; left: 20px;';
   document.getElementById('fieldSpot1').appendChild(img);
 }
+
+let microscope = document.getElementById('Microscope');
+microscope.onclick = () => {
+  geneViewMode = true;
+};
